927a25d754b9d130ee7d984e2cf183c506c45294
ryo <68486607+swiftryo@users.noreply.github.com>
2021-01-31 11:52:28 +0900

    add Follow button

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,13 @@ import './bootstrap'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike'
 import ArticleTagsInput from './components/ArticleTagsInput.vue'
+import FollowButton from './components/FollowButton.vue'
 
 const app = new Vue({
   el: '#app',
   components: {
     ArticleLike,
     ArticleTagsInput,
+    FollowButton,
   }
 })
